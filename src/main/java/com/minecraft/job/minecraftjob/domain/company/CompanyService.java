c93caa92d148de46c4c5d90f796922a72b0f98c8
peubel <sksqkrwns@naver.com>
2022-09-24 18:06:40 +0900

    MIN-12 jpa 이전 작업 오류 해결

--- src/main/java/com/minecraft/job/minecraftjob/domain/company/CompanyService.java
+++
@@ -1,6 +0,0 @@
-package com.minecraft.job.minecraftjob.domain.company;
-
-public interface CompanyService {
-
-    void create(String email, String password, String name, String description, String logo);
-}
