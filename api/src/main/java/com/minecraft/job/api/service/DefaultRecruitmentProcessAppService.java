236ff1f3fc93155fe44c40f32b99c26f82f48759
bgm <qkrrudals2403@naver.com>
2022-10-20 21:40:40 +0900

    MIN-86 채용과정 생성 이벤트 개발

---
+++ api/src/main/java/com/minecraft/job/api/service/DefaultRecruitmentProcessAppService.java
@@ -0,0 +1,44 @@
+package com.minecraft.job.api.service;
+
+import com.minecraft.job.common.recruitment.domain.Recruitment;
+import com.minecraft.job.common.recruitmentProcess.domain.RecruitmentProcess;
+import com.minecraft.job.common.recruitmentProcess.domain.RecruitmentProcessCreateEvent;
+import com.minecraft.job.common.recruitmentProcess.domain.RecruitmentProcessRepository;
+import com.minecraft.job.integration.mail.Mail;
+import com.minecraft.job.integration.mail.MailApi;
+import com.minecraft.job.integration.mail.MailTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+import java.util.Map;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class DefaultRecruitmentProcessAppService implements RecruitmentProcessAppService {
+
+    private final RecruitmentProcessRepository recruitmentProcessRepository;
+    private final MailApi mailApi;
+
+    @Async
+    @TransactionalEventListener(RecruitmentProcessCreateEvent.class)
+    public void onCreateRecruitmentProcessListener(RecruitmentProcessCreateEvent event) throws Exception {
+        RecruitmentProcess recruitmentProcess = recruitmentProcessRepository.findById(event.recruitmentProcessId()).orElseThrow();
+        Recruitment recruitment = recruitmentProcess.getRecruitment();
+
+        String recruitmentName = recruitment.getTitle();
+        String leaderEmail = recruitment.getTeamName();
+        String teamName = recruitment.getTeamOfLeaderEmail();
+        String userNickname = recruitmentProcess.getUserNickname();
+
+        mailApi.send(new Mail(
+                new String[]{leaderEmail},
+                MailTemplate.REVIEW_CREATE,
+                Map.of("teamName", teamName, "userNickname", userNickname,
+                        "recruitmentName", recruitmentName)
+        ));
+    }
+}
