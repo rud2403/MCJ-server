26b61b4a64d2a3d1ba89e9496ce6ead56a54a76e
Jun, Ahah <96584832+peubel@users.noreply.github.com>
2023-01-19 23:07:07 +0900

    MIN-149 EmailAuth 검증 테스트 추가

--- common/src/test/java/com/minecraft/job/common/emailauth/domain/EmailAuthTest.java
+++ common/src/test/java/com/minecraft/job/common/emailauth/domain/EmailAuthTest.java
@@ -1,8 +1,11 @@
 package com.minecraft.job.common.emailauth.domain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class EmailAuthTest {
 
@@ -11,5 +14,11 @@ class EmailAuthTest {
         EmailAuth emailAuth = EmailAuth.create("email");
 
         assertThat(emailAuth.getEmail()).isEqualTo("email");
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource()
+    void 이메일_인증_생성_실패(String email) {
+        assertThatIllegalArgumentException().isThrownBy(() -> EmailAuth.create(email));
     }
 }
