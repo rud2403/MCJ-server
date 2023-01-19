dfcea8b19e5722849cc38a09cc620a45d3b773dd
Jun, Ahah <96584832+peubel@users.noreply.github.com>
2023-01-19 23:04:58 +0900

    MIN-149 인코딩 오류 재해결

--- common/src/test/java/com/minecraft/job/common/emailauth/domain/EmailAuthTest.java
+++ common/src/test/java/com/minecraft/job/common/emailauth/domain/EmailAuthTest.java
@@ -1,1 +1,15 @@
-package com.minecraft.job.common.emailauth.domain;import org.junit.jupiter.api.Test;import static org.assertj.core.api.Assertions.assertThat;class EmailAuthTest {    @Test    void 이메일_인증_생성() {        EmailAuth emailAuth = EmailAuth.create("email");        assertThat(emailAuth.getEmail()).isEqualTo("email");    }}
+package com.minecraft.job.common.emailauth.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EmailAuthTest {
+
+    @Test
+    void 이메일_인증_생성() {
+        EmailAuth emailAuth = EmailAuth.create("email");
+
+        assertThat(emailAuth.getEmail()).isEqualTo("email");
+    }
+}
