03f34fead27c2dc93776f562f459d069682ef73f
Jun, Ahah <96584832+peubel@users.noreply.github.com>
2023-03-19 15:43:55 +0900

    MCJ-3 advice에 인증 예외 추가

--- api/src/main/java/com/minecraft/job/api/controller/ApiAdvice.java
+++ api/src/main/java/com/minecraft/job/api/controller/ApiAdvice.java
@@ -1,14 +1,18 @@
 package com.minecraft.job.api.controller;
 
+import com.minecraft.job.api.security.user.McjUserException;
 import com.minecraft.job.common.support.MinecraftJobException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Locale;
+
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 @RestControllerAdvice
 @RequiredArgsConstructor
@@ -24,6 +28,14 @@ public class ApiAdvice {
     @ExceptionHandler(MinecraftJobException.class)
     public ErrorResponse exception(MinecraftJobException ex) {
         final String message = messageSource.getMessage(ex.getMessage(), null, Locale.getDefault());
+
+        return new ErrorResponse(new ErrorData(message));
+    }
+
+    @ResponseStatus(UNAUTHORIZED)
+    @ExceptionHandler(McjUserException.class)
+    public ErrorResponse exception(McjUserException ex) {
+        final String message = ex.getMessage();
 
         return new ErrorResponse(new ErrorData(message));
     }
